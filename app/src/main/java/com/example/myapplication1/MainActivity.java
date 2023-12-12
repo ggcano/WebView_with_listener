6ff6a652bf7713e08c1ca06f16b447fed51546d7
Adrian_cano <ggcano@gmx.es>
2023-12-12 21:13:24 +0100

    init

--- app/src/main/java/com/example/myapplication1/MainActivity.java
+++ app/src/main/java/com/example/myapplication1/MainActivity.java
@@ -8,7 +8,7 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.webkit.DownloadListener;
 
-import android.webkit.MimeTypeMap;
+import android.webkit.URLUtil;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -20,7 +20,8 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setupWebView(webView);
+        //setupWebView(webView);
+        setupWebViewWithDownloadManager(webView);
     }
 
 
@@ -47,8 +48,6 @@ public class MainActivity extends Activity {
             public void onDownloadStart(String url, String userAgent,
                                         String contentDisposition, String mimetype,
                                         long contentLength) {
-
-
                 Intent i = new Intent(Intent.ACTION_VIEW);
                 i.setData(Uri.parse(url));
                 startActivity(i);
@@ -58,5 +57,31 @@ public class MainActivity extends Activity {
 
     }
 
+    private void setupWebViewWithDownloadManager(WebView myWebView) {
+        myWebView = (WebView) findViewById(R.id.webiview);
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                if (url.endsWith(".pdf") || url.endsWith(".doc") || url.endsWith(".xls") || url.endsWith(".ppt")) {
+                    DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+                    request.allowScanningByMediaScanner();
+                    request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                    String fileName = URLUtil.guessFileName(url, null, null);
+                    request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, fileName);
+
+                    DownloadManager dm = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+                    dm.enqueue(request);
+                    return true;
+                }
+                view.loadUrl(url);
+                return true;
+            }
+        });
+
+        myWebView.loadUrl("https://www.sspa.juntadeandalucia.es/servicioandaluzdesalud/documentos");
+    }
 
 }
+
+
